ffbb23853f4cd1a0c26c8b43848ac272452aad8c
Sean Stirling <sean.a.stirling@gmail.com>
2018-11-11 11:25:12 +0000

    Added the bank screen Added the optional features screen Added the personal info screen Added the statistics screen

---
+++ app/src/main/java/com/kiwi/moon/coinz/OptionalFeaturesActivity.java
@@ -0,0 +1,13 @@
+package com.kiwi.moon.coinz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OptionalFeaturesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_optional_features);
+    }
+}
