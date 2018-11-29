261fce8f2cc8a79a202d5938fc8e5bbac29446a2
Sean Stirling <sean.a.stirling@gmail.com>
2018-11-29 14:27:36 +0000

    Designed Optional Features screen

--- app/src/main/java/com/kiwi/moon/coinz/OptionalFeaturesActivity.java
+++ app/src/main/java/com/kiwi/moon/coinz/OptionalFeaturesActivity.java
@@ -2,6 +2,9 @@ package com.kiwi.moon.coinz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.EditText;
 
 public class OptionalFeaturesActivity extends AppCompatActivity {
 
@@ -9,5 +12,34 @@ public class OptionalFeaturesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_optional_features);
+
+        //If Ghost card is pressed
+        final CardView cardGhost = findViewById(R.id.cardGhost);
+        cardGhost.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                //Call function to activate ghost mode
+                activateGhost();
+            }
+        });
+
+        final CardView cardTimeTrial = findViewById(R.id.cardTimeTrial);
+        cardTimeTrial.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                //Call function to activate Time Trial
+                activateTimeTrial();
+            }
+        });
+    }
+
+    public void activateTimeTrial(){
+
+    }
+
+    public void activateGhost(){
+
     }
 }
