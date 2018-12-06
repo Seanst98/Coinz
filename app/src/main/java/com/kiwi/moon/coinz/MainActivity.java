713cbd02ba1ac0b20e527fdb0f8400e6fb543cfe
Sean Stirling <sean.a.stirling@gmail.com>
2018-12-06 10:37:39 +0000

    Removed all warnings from MainActivity

--- app/src/main/java/com/kiwi/moon/coinz/MainActivity.java
+++ app/src/main/java/com/kiwi/moon/coinz/MainActivity.java
@@ -2,8 +2,6 @@ package com.kiwi.moon.coinz;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -17,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
